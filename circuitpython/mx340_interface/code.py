2e146fd4308a38b01e06f09c42925ac8b4b3d39d
Roger Cheng <roger.random@outlook.com>
2024-05-27 10:51:55 -0700

    UART receiver loop

--- circuitpython/mx340_interface/code.py
+++ circuitpython/mx340_interface/code.py
@@ -43,29 +43,59 @@ async def blink_rgb(pin, brightness_level, on_time, off_time, pause_time):
             px.fill((0, 0, 0))
             await asyncio.sleep(pause_time)
 
-# A check to see if we're getting the data stream expected to be coming steadily from K13988 chip
-async def startup_check(uart):
-    while uart.in_waiting < 2:
-        await asyncio.sleep(0)
-    data = uart.read(2)
-    assert data is not None
-    if data == b'\x80\x40':
-        print("Startup check: 0x80 0x40 as expected")
-        return
-    elif data == b'\x40\x80':
-        print("Startup check: 0x40 0x80 might just be caused by tuning in at wrong time. Acceptable.")
-        return
-    elif data == b'\x80\x80':
-        print("Startup check: 0x80 and it appears 0x40 has already been turned off. Acceptable.")
-        return
-    else:
-        raise RuntimeError("Did not see byte sequence (0x80 0x40 or just 0x80) expected from K13988")
+# Class that represents latest data heard from K13988
+class ReceivedData:
+    def __init__(self):
+        self.last_report = 0x80
+        self.report_unchanged_count = 0
+        self.ack_count = 0
+        self.ignored0x40_count = 0
+
+# Data receive loop
+async def uart_receiver(uart, rx_data):
+    assert uart is not None
+    assert rx_data is not None
+
+    while True:
+        while uart.in_waiting < 1:
+            await asyncio.sleep(0)
+        data = uart.read(1)[0]
+
+        if data == 0x20:
+            rx_data.ack_count += 1
+        elif data == 0x40:
+            rx_data.ignored0x40_count += 1
+        elif data == rx_data.last_report:
+            rx_data.report_unchanged_count += 1
+        else:
+            rx_data_print(rx_data)
+            rx_data.last_report = data
+            rx_data.report_unchanged_count = 0
+
+# Print ReceivedData values to console
+def rx_data_print(rx_data):
+    print(f"0x{rx_data.last_report:x} for {rx_data.report_unchanged_count} / {rx_data.ack_count} ACK / {rx_data.ignored0x40_count} 0x40 ignored")
+    rx_data.report_unchanged_count = 0
+    rx_data.ack_count = 0
+    rx_data.ignored0x40_count = 0
+
+# Periodically dump ReceivedData values
+async def rx_data_print_loop(rx_data):
+    assert rx_data is not None
+
+    while True:
+        await asyncio.sleep(1.0)
+        rx_data_print(rx_data)
 
 # UART communication with K13988 chip
 async def k13988(tx, rx, enable):
+    rx_data = ReceivedData()
+
     with digitalio.DigitalInOut(enable) as enable_pin, busio.UART(board.TX, board.RX, baudrate=250000, bits=8, parity=busio.UART.Parity.EVEN, stop=1, timeout=20) as uart:
         enable_pin.switch_to_output(True)
-        await startup_check(uart)
+        receiver = asyncio.create_task(uart_receiver(uart, rx_data))
+        printer = asyncio.create_task(rx_data_print_loop(rx_data))
+        await asyncio.gather(receiver, printer)
 
 async def main():
     led_task = asyncio.create_task(blink_rgb(board.NEOPIXEL, 25, 0.05, 0.1, 1.0))
